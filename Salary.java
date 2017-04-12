382d288ae0fed9598825fb53df6e56ee9b4b3c12
paulemboli <iampaulemboli@gmail.com>
2017-04-11 23:12:35 -0500

    Add files via upload

---
+++ Salary.java
@@ -0,0 +1,16 @@
+package Review.java;
+
+public class Salary {
+
+	public static void main(String[] args) {
+		 
+		int wage= 20;
+		
+		System.out.print("Wage is: ");
+		System.out.println(wage);
+		System.out.println("Goodbye");
+		
+		return;
+	}
+
+}
